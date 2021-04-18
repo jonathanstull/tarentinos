72ad362837e857c3d9177a9ab2315df553fbcc11
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-18 12:50:08 -0700

    pass third test, sum the total cost

--- src/main.js
+++ src/main.js
@@ -12,16 +12,22 @@ class Pizza {
     this.artichoke = artichokeCost;
     this.anchovy = anchovyCost;
     this.tomato = tomatoCost;
+    this.itemCosts;
+    this.totalCost;
   }
 
   addItemCost() {
-    let itemCosts = [];
-    itemCosts.push(Object.values(this));
-    return itemCosts;
+    let itemCosts = Object.values(this);
+    return this.itemCosts = itemCosts;
   }
 
   sumTotalCost() {
-
+    let itemCostsArray = this.itemCosts;
+    let totalCost = 0;
+    itemCostsArray.forEach((itemCost) => {
+      totalCost += itemCost;
+    });
+    return this.totalCost = totalCost;
   }
 }
 
