1613cab3284ba62dbe849ae34588f88a924c1df0
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-18 12:17:14 -0700

    refactor HTML with price scheme and adjust project file structure with main.js

--- src/main.js
+++ src/main.js
@@ -2,16 +2,16 @@
 
 class Pizza {
 
-  constructor(size, crust, cheese, sauce, meat, mushrooms, artichoke, anchovy, tomato) {
-    this.size = size;
-    this.crust = crust;
-    this.cheese = cheese;
-    this.sauce = sauce;
-    this.meat = meat;
-    this.mushrooms = mushrooms;
-    this.artichoke = artichoke;
-    this.anchovy = anchovy;
-    this.tomato = tomato;
+  constructor(sizeCost, crustCost, cheeseCost, sauceCost, meatCost, mushroomsCost, artichokeCost, anchovyCost, tomatoCost) {
+    this.size = sizeCost;
+    this.crust = crustCost;
+    this.cheese = cheeseCost;
+    this.sauce = sauceCost;
+    this.meat = meatCost;
+    this.mushrooms = mushroomsCost;
+    this.artichoke = artichokeCost;
+    this.anchovy = anchovyCost;
+    this.tomato = tomatoCost;
   }
 
 }
