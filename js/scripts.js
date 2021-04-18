045a17c09c55b4867db09c617719302bbbb00a5e
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-18 10:30:39 -0700

    initial refactor commit: add whiteboard.md with outline of project refactor

--- js/scripts.js
+++ js/scripts.js
@@ -2,79 +2,105 @@
 
 let tarentinos = new Tarentinos();
 
-function Tarentinos() {
-  this.orders = {};
-  this.currentOrderId = 0;
-}
 
-Tarentinos.prototype.addOrder = function(order) {
-  order.orderId = this.assignOrderId();
-  this.orders[order.orderId] = order;
-};
 
-Tarentinos.prototype.assignOrderId = function() {
-  this.currentOrderId += 1;
-  return this.currentOrderId;
-};
+// function Tarentinos() {
+//   this.orders = {};
+//   this.currentOrderId = 0;
+// }
 
-// Business logic for Order
+// Tarentinos.prototype.addOrder = function(order) {
+//   order.orderId = this.assignOrderId();
+//   this.orders[order.orderId] = order;
+// };
 
-function Order() {
-  this.orderId = 1;
-  this.crusts = {};
-  this.sizes = {};
-  this.cheeses = {};
-  this.toppings = {};
-  this.sides = {};
-  this.aficionado = {};
-};
+// Tarentinos.prototype.assignOrderId = function() {
+//   this.currentOrderId += 1;
+//   return this.currentOrderId;
+// };
 
-Order.prototype.addAficionado = function(aficionado) {
-  this.aficionado[aficionado.name] = aficionado;
-};
+// // Business logic for Order
 
-Order.prototype.addPizza = function(size, crust, cheeses, toppings, sides) {
-  this.sizes["size"] = size;
-  this.crusts["crust"] = crust;
-  this.cheeses["cheeses"] = cheeses;
-  this.toppings["toppings"] = toppings;
-  this.sides["sides"] = sides;
-};
+// function Order() {
+//   this.orderId = 1;
+//   this.crusts = {};
+//   this.sizes = {};
+//   this.cheeses = {};
+//   this.toppings = {};
+//   this.sides = {};
+//   this.aficionado = {};
+// };
 
-// Business logic for Aficionado
+// Order.prototype.addAficionado = function(aficionado) {
+//   this.aficionado[aficionado.name] = aficionado;
+// };
 
-function Aficionado(name, address, city, state, zip, phone, email) {
-  this.name = name;
-  this.address = address;
-  this.city = city;
-  this.state = state;
-  this.zip = zip;
-  this.phone = phone;
-  this.email = email;
-};
+// Order.prototype.addPizza = function(size, crust, cheeses, toppings, sides) {
+//   this.sizes["size"] = size;
+//   this.crusts["crust"] = crust;
+//   this.cheeses["cheeses"] = cheeses;
+//   this.toppings["toppings"] = toppings;
+//   this.sides["sides"] = sides;
+// };
+
+// // Business logic for Aficionado
+
+// function Aficionado(name, address, city, state, zip, phone, email) {
+//   this.name = name;
+//   this.address = address;
+//   this.city = city;
+//   this.state = state;
+//   this.zip = zip;
+//   this.phone = phone;
+//   this.email = email;
+// };
 
 // Business logic for order listeners
 
-function addOrderListeners() {
-  let checkboxes = $("input[name:cheeses]");
-  checkboxes.change(function() {
+// function addOrderListeners() {
+//   let checkboxes = $("input[name:cheeses]");
+//   checkboxes.change(function() {
 
-});
+// });
+
+// Business logic for Cheeses
+
+function getCheeses() {
+  let cheeses = {};
+  $("input[name:cheeses]").forEach(function() {
+    if ($("#mozzarella").val() !== undefined) {
+      cheeses["mozzarella"] = true;
+    } else if ($("#feta").val() !== undefined) {
+      cheeses["feta"] = true;
+    } else if ($("#havarti").val() !== undefined) {
+      cheeses["havarti"] = true;
+    } else if ($("#pecorinoRomano").val() !== undefined) {
+      cheeses["pecorinoRomano"] = true;
+    };
+  });
+  return cheeses
+};
 
 // User interface logic
 
 $(document).ready(function() {
   $("#order-form").submit(function(event) {
-    event.preventDefault();
+    console.log(event)
+      event.preventDefault();
+    // debugger
     // addOrderListeners();
     // console.log(cheeses);
 
-    const size = $("input:radio[name=sizes]").val();
-    console.log(size);
-    const crust = $("input:radio[name=crusts]").val();
-    console.log(crust);
-
+    // const size = $("input:radio[name=sizes]").val();
+    // console.log(size);
+    // const crust = $("input:radio[name=crusts]").val();
+    // console.log(crust);
+    // let cheeses = {};
+    // getCheeses();
+    // console.log(cheeses);
+  
     const name = $("#name").val();
+    console.log(name);
     const address = $("#address").val();
     const city = $("#city").val();
     const state = $("#state").val();
@@ -83,107 +109,5 @@ $(document).ready(function() {
     const email = $("#email").val();
     let newAficionado = new Aficionado(name, address, city, state, zip, phone, email);
     console.log(newAficionado);
-    
   });
 });
-
-// function addCheeses() {
-//   if ($("#mozzarella").is(":checked") === true) {
-//     cheeses["mozzarella"] = true;
-//   } else if ($("#feta").is(":checked") === true) {
-//     let cheeses["feta"] = true;
-//   } else if ($("#havarti").is(":checked") === true) {
-//     cheeses["havarti"] = true;
-//   } else if ($("#pecorinoRomano").is(":checked") === true) {
-//     cheeses["havarti"] = true;
-//   }
-//   return cheeses;
-// };
-
-// js architecture **PSEUDO**
-
-// tarentinos {
-//   newOrder {
-//     // create a key-value object that allows a user to interact with check boxes on a form
-//     crusts {
-//       deepDish: false,
-//       pan: false,
-//       traditional: false
-//     }
-//     sizes {
-//       inches8: false,
-//       inches12: false,
-//       inches16: false
-//     }
-//     cheeses {
-//       mozzarella: false,
-//       feta: false,
-//       havarti: false,
-//       pecorinoRomano: false
-//     }
-//     toppings {
-//       italianSausage: false,
-//       chickenSausage: false,
-//       mushrooms: false,
-//       lilMamas: false,
-//       canadaBacon: false,
-//       pepperoni: false,
-//       pineapple: never // What kind of barbarian would put pineapple on their pizza?!?
-//     }
-//     sides {
-//       chiliFlake: false,
-//       ranch: false,
-//       marinara: false,
-//       garlicButter: false
-//   }
-//   newOrder.method that toggles the Boolean values for menu options
-//   newOrder constructor that passes true Boolean values for toppings from menu to customer
-//   newOrder.method TBD
-
-//   aficionado {
-//     this.name = name,
-//     this.street = street,
-//     this.city = city,
-//     this.state = state,
-//     this.zip = zip,
-//     this.phone = phone,
-//     this.email = email
-//     this.aficionadoOrder = {
-//       crusts {
-//         deepDish: false,
-//         thinCrust: false,
-//         thickCrust: false
-//       }
-//       cheeses {
-//         mozzarella: false,
-//         feta: false,
-//         havarti: false,
-//         pecorinoRomano: false
-//       }
-//       sizes {
-//         inches8: false,
-//         inches12: false,
-//         inches16: false
-//       }
-//       toppings {
-//         italianSausage: false,
-//         chickenSausage: false,
-//         mushrooms: false,
-//         lilMamas: false,
-//         canadaBacon: false,
-//         pepperoni: false,
-//         pineapple: never // What kind of barbarian would put pineapple on their pizza?!?
-//       }
-//       sides {
-//         chiliFlake: false,
-//         ranch: false,
-//         marinara: false,
-//         garlicButter: false
-//     }
-//   }
-//   aficionado constructor
-//   aficionado.method that calculates the cost
-
-// tarentinos.method TBD
-// tarentinos.method TBD
-// }
---
+++ whiteboard.md
@@ -0,0 +1,12 @@
+Pizza Parlor
+Create a website for a pizza company where a user can choose one or more individual toppings (cheese, pepperoni, artichoke, anchovy, etc) and a size to order a pizza and see the final cost.
+
+Allow the user to choose toppings and size for the pizza they'd like to order.
+Create a pizza object constructor with properties for toppings and size.
+Create a prototype method for the cost of a pizza depending on the selections chosen. Use your own formula for this.
+Keep it simple to start! You do not need to have a complex formula for determining cost (although you can if you like). The most important part of this code review is using and demonstrating your understanding of constructors and prototypes.
+
+// user-submitted form with drop-down menus for pizza toppings
+// constructor for a pizza including nested objects for toppings
+// each topping is an array consisting of a value push from a branch that determines cost the cost based on user input
+// the final equation will sum the values in the array using a forEach loop and return the cost of the pizza
